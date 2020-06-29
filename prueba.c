4e6c8e1b765fc4c7dd574642164b545fd98353bf
Miguel Hernández Gaitan <60478285+MiguelHG2351@users.noreply.github.com>
2020-06-29 11:44:08 -0600

    SAVEdfvwef

--- prueba.c
+++ prueba.c
@@ -3,9 +3,10 @@
 int main()
 {
 int array[100], n, c, d, swap;
-int users[5] = {"Norwin", "Python", "React JS", "JavaScript", "NodeJS"};
-int notas[5] = {0, 0, 0, 0, 0};
-printf("Bienvenido a la escuela Dennis Ritchie");
+int users[4] = {"Norwin", "Python", "React JS", "JavaScript", "NodeJS"};
+float notas[4] = {0, 0, 0, 0, 0};
+float media;
+printf("Bienvenido a la escuela Dennis Ritchie\n");
 printf("Ingrese el nombre del estudiante\n");
 scanf("%d", &n);
 printf("Enter %d integers\n", n);
@@ -24,6 +25,11 @@ for (d = 0 ; d < n - c - 1; d++)
   }
 }
 
+media = notas[0] + notas[1] + notas[2] + notas[3] + notas[4];
+media = media / 5;
+printf("La media de la nota es: ");
+printf("%i", media);
+getch();
 printf("Sorted list in ascending order:\n");
 
 for ( c = 0 ; c < n ; c++ )
